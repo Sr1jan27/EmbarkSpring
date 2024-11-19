6a18acaa29f8cd951197d88785cb7a6f8cf77831
Sr1jan27 <singhsrijan809@gmail.com>
2024-11-19 08:29:27 +0530

    first commit for Embark Springboot project

---
+++ src/main/java/org/srijan/embarkspring/EmbarkSpringApplication.java
@@ -0,0 +1,13 @@
+package org.srijan.embarkspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbarkSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmbarkSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/srijan/embarkspring/EmbarkSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.srijan.embarkspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbarkSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
