4471872f2bb0e9e62aeb77b28adc57c5ca12cce7
Sr1jan27 <singhsrijan809@gmail.com>
2024-11-19 08:51:05 +0530

    trying without sout and request mapping

--- src/main/java/org/srijan/embarkspring/HelloController.java
+++ src/main/java/org/srijan/embarkspring/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@RequestMapping("/kilo")
+@RequestMapping("/kilo")
 public class HelloController {
 
     @GetMapping("/world")
